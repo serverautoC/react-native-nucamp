e7a9c5efcbeea3f96bfd5910d4ed07056bcc1b27
serverautoC <113566854+serverautoC@users.noreply.github.com>
2022-12-17 23:14:08 -0500

    Add files via upload

---
+++ screens/ContactScreen.js
@@ -0,0 +1,24 @@
+import { ScrollView, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+
+const ContactScreen = () => {
+
+    return (
+        <ScrollView>
+            <Card wrapperStyle={{margin: 20}}>
+                <Card.Title>Contact Information
+                    </Card.Title>
+                    <Card.Divider>
+                      <Text> 1 Nucamp Way</Text>  
+                      <Text>Seattle, WA 98001</Text>
+                        <Text>U.S.A.</Text>
+                        <Text style={{marginBottom: 10 }}></Text>
+                      <Text> Phone: 1-206-555-1234</Text> 
+                      <Text>Email: campsites@nucamp.co</Text>  
+                      </Card.Divider>   
+            </Card>
+        </ScrollView>
+    )
+}
+export default ContactScreen;
